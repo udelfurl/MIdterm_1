688c96c1c4400fc7818ea5efb16764276bae862e
Robert Furlong <stefurlr@udel.edu>
2015-03-17 15:02:04 -0400

    changed some mistakes

--- src/main/Student.java
+++ src/main/Student.java
@@ -1,13 +1,18 @@
 package main;
-
+import java.util.Scanner;
 public class Student extends Person {
 	private final String class_status;
 	public Student(String class_status){
+		Scanner stu=new Scanner(System.in);
+		System.out.println("Enter you class status(Freshman,Sophmore,Junior,Senior): ");
+		class_status= stu.toString();
 		this.class_status=class_status;
 				}
 	public String getClassStatus(){
 		return class_status;
 	}
-	
+	public String Student(String name, String phone,String address, String email){
+		return "name,phone,address,email";
+	}
 
 }
